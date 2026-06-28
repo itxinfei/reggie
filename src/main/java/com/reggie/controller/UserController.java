41dd73aaffe5fe134d186b8955f193b986c2a09e
心飞为你飞 <17671787093@163.com>
2026-06-28 17:25:42 +0800

    feat: 移动端退出登录

--- src/main/java/com/reggie/controller/UserController.java
+++ src/main/java/com/reggie/controller/UserController.java
@@ -90,4 +90,10 @@ public class UserController {
         return R.error("登录失败");
     }
 
+    @PostMapping("/loginout")
+    public R<String> loginout(HttpSession session) {
+        session.removeAttribute("user");
+        return R.success("退出成功");
+    }
+
 }
