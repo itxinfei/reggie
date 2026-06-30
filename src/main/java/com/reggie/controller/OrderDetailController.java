8728263a832fc73dcbec4923324727d9cd27aab7
心飞为你飞 <17671787093@163.com>
2026-06-30 17:19:44 +0800

    docs: add Swagger annotations to OrderDetailController

--- src/main/java/com/reggie/controller/OrderDetailController.java
+++ src/main/java/com/reggie/controller/OrderDetailController.java
@@ -3,6 +3,9 @@ package com.reggie.controller;
 import com.reggie.common.R;
 import com.reggie.entity.OrderDetail;
 import com.reggie.service.OrderDetailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +16,15 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/orderDetail")
+@Tag(name = "订单明细", description = "订单明细查询接口")
 public class OrderDetailController {
 
     @Autowired
     private OrderDetailService orderDetailService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "查询订单明细", description = "根据ID查询订单明细详情")
+    @Parameter(name = "id", description = "订单明细ID", required = true)
     public R<OrderDetail> get(@PathVariable Long id) {
         OrderDetail orderDetail = orderDetailService.getById(id);
         if (orderDetail != null) {
