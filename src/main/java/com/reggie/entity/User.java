07fc9e78bf567ba7d8c4a2ccf3b0b9b3cfc1d84b
心飞为你飞 <17671787093@163.com>
2026-06-30 15:50:40 +0800

    feat: add parameter validation to User entity

--- src/main/java/com/reggie/entity/User.java
+++ src/main/java/com/reggie/entity/User.java
@@ -3,6 +3,9 @@ package com.reggie.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -19,9 +22,13 @@ public class User implements Serializable {
     private Long tenantId;
 
     //姓名
+    @NotBlank(message = "姓名不能为空")
+    @Size(max = 30, message = "姓名不能超过30位")
     private String name;
 
     //手机号
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
     private String phone;
 
     //性别 0 女 1 男
