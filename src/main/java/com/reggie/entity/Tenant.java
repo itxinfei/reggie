cee0ab9bd23c82571ac9d1314148f86a78e2abf0
心飞为你飞 <17671787093@163.com>
2026-06-30 15:20:38 +0800

    feat: add passwordType field to Tenant entity

--- src/main/java/com/reggie/entity/Tenant.java
+++ src/main/java/com/reggie/entity/Tenant.java
@@ -3,6 +3,7 @@ package com.reggie.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.reggie.common.SecurityConstants;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -18,6 +19,11 @@ public class Tenant implements Serializable {
     private String phone;
     private String address;
     private Integer status;
+
+    /**
+     * 密码加密类型：MD5、BCRYPT
+     */
+    private String passwordType = SecurityConstants.PASSWORD_TYPE_MD5; // 默认MD5，兼容老数据
 
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
